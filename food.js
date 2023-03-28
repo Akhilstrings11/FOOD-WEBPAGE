f2ce3b131500c53ab61774752a6eb7deba985f29
Akhilstrings11 <125636435+Akhilstrings11@users.noreply.github.com>
2023-03-28 09:38:53 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,17 @@
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
+
+sideNav.style.right = "-250px";
+
+menuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+        menu.src = "images/close.png";
+    } 
+    else{
+        sideNav.style.right = "-250px";
+        menu.src = "images/menu.png";
+
+    }
+}
